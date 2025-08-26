0bba0a1003a561491ea5615566888e5a27c680ba
CraftKontrol <arnaud.cassone@gmail.com>
2025-08-26 11:21:00 +0200

    Add SplatMap extension and update node files
    Introduced SplatMapExt.py to extend SplatMap node functionality, including dynamic menu updates and parameter management. Updated README to version 1.049, modified SplatMap.tox binary, and added .gitignore for backup exclusion.

---
+++ Nodes/SplatMap/SplatMapExt.py
@@ -0,0 +1,81 @@
+"""
+SplatMap extension module
+Author: Arnaud Cassone
+
+"""
+
+from TDStoreTools import StorageManager
+import TDFunctions as TDF
+import CraftGGenUtils
+
+class SplatMapExt:
+	"""
+	SplatMapExt is an extension for the SplatMap node.
+	"""
+	def __init__(self, ownerComp):
+		# The component to which this extension is attached
+		self.ownerComp = ownerComp
+
+		# properties
+		TDF.createProperty(self, 'SourceMenuNames', value=[], dependable=True,readOnly=False)
+
+	def OnStart(self):
+		
+		# Gather menu names from the 'attributes' DAT table
+		tempList = []
+		datmenu = op('attributes')
+		for i in range(datmenu.numRows - 1):
+			tempList.append(datmenu[i+1, 0].val)
+
+		# Update SourceMenuNames and menu if changed
+		if self.SourceMenuNames != tempList:
+			self.SourceMenuNames = tempList
+			self.updateMenu()	
+		
+		# Update parameter name and source
+		self.changeParamName(parent().par.Paramname)
+		self.changeSource(parent().par.Source)
+	
+		pass
+
+	def updateMenu(self):
+		# update the menu of the source parameter
+		menu = parent().par.Source
+		menu.menuNames = self.SourceMenuNames
+		menu.menuLabels = self.SourceMenuNames
+
+		pass	
+
+	def Update(self):
+		
+		# Gather menu names from the 'attributes' DAT table
+		tempList = []
+		datmenu = op('attributes')
+		for i in range(datmenu.numRows - 1):
+			tempList.append(datmenu[i+1, 0].val)
+
+		# Update SourceMenuNames and menu if changed
+		if self.SourceMenuNames != tempList:
+			self.SourceMenuNames = tempList
+			self.updateMenu()	
+		pass
+
+	def OnChange(self, name, value, prev):	
+		pass
+
+	def OnCreate(self):
+		# Manage params of the cloned operator
+		if "GGenFamily" not in parent().tags:
+			op = parent()
+			if op:
+				CraftGGenUtils.SetCloneParameters(op)
+		pass
+
+	
+	def OnPulse(self, name):
+		if name == 'Help':
+			# open html browser
+			mod.webbrowser.open('https://github.com/CraftKontrol/GroundGen-for-Touchdesigner/wiki/splatmap')
+		pass
+
+	

52dfb0e9f63a862411a0b74ea5ba1eda30beaba8
CraftKontrol <arnaud.cassone@gmail.com>
2025-08-26 11:07:57 +0200

    Add SlopeExt extension and update Slope node
    Introduces SlopeExt.py as an extension module for the Slope node, providing hooks for lifecycle events and parameter management. Updates Slope.tox binary, bumps README version to 1.022, and refines .gitignore formatting.

--- Nodes/Slope/README.md
+++ Nodes/Slope/README.md
@@ -1,4 +1,4 @@
 #Slope
-#Version 1.021
+#Version 1.022
 
 Perform a Cavity using neighboring.
---
+++ Nodes/Slope/SlopeExt.py
@@ -0,0 +1,39 @@
+"""
+Slope Extension Module
+Author: Arnaud Cassone
+
+"""
+
+from TDStoreTools import StorageManager
+import TDFunctions as TDF
+import CraftGGenUtils
+
+class SlopeExt:
+	"""
+	SlopeExt is an extension for the Slope node.
+	"""
+	def __init__(self, ownerComp):
+		# The component to which this extension is attached
+		self.ownerComp = ownerComp
+
+	def OnStart(self):
+		pass
+
+	def OnChange(self, name, value, prev):
+		pass
+	
+	def OnCreate(self):
+
+		# Manage params of the cloned operator
+		if "GGenFamily" not in parent().tags:
+			op = parent()
+			if op:
+				CraftGGenUtils.SetCloneParameters(op)
+		pass
+	
+	def OnPulse(self, name):
+		if name == 'Help':
+			# open html browser
+			mod.webbrowser.open('https://github.com/CraftKontrol/GroundGen-for-Touchdesigner/wiki/slope')
+		pass
+	

387f1e7b5cb0b940f1ddb55c631722ef601289c3
CraftKontrol <arnaud.cassone@gmail.com>
2025-08-26 09:56:04 +0200

    Externalized BeachExt

---
+++ Nodes/Beach/BeachExt.py
@@ -0,0 +1,41 @@
+"""
+Beach Extension Module
+Author: Arnaud Cassone
+
+"""
+from TDStoreTools import StorageManager
+import TDFunctions as TDF
+import CraftGGenUtils
+class BeachExt:
+	"""
+	BeachExt is an extension module for the Beach node.
+	
+	"""
+	def __init__(self, ownerComp):
+		# The component to which this extension is attached
+		self.ownerComp = ownerComp
+
+	def myFunction(self, v):
+		debug(v)
+
+	def OnStart(self):
+		pass
+	
+	def OnCreate(self):
+		# Manage params of the cloned operator
+		if "GGenFamily" not in parent().tags: 
+			op = parent()
+			if op:
+				CraftGGenUtils.SetCloneParameters(op)
+
+		pass
+	
+	def OnPulse(self, name):
+		if name == 'Help':
+			# open html browser
+			mod.webbrowser.open('https://github.com/CraftKontrol/GroundGen-for-Touchdesigner/wiki/beach')
+		pass
+	
+	def OnChange(self, name, value, prev):
+		pass
+	
--- Nodes/Beach/README.md
+++ Nodes/Beach/README.md
@@ -1,3 +1,5 @@
 #Beach
 #Version 1.024
-A Beach Smoothing
+A Beach Smoothing Node.
+This node will smooth the terrain to create a beach effect between water and land.
+It is recommended to use this node before any erosion or sediment transport nodes.
